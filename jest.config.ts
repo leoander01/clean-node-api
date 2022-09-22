ded750ec486e677256e4de516cf4cca2f5bb670f
leoander01 <leoanderneves8@outlook.com>
2022-09-22 10:39:13 -0300

    refactor: removing collectCoverage to not show too much information in tests

--- jest.config.ts
+++ jest.config.ts
@@ -2,9 +2,6 @@ export default {
   // A list of paths to directories that Jest should use to search for files in
   roots: ['<rootDir>/src'],
 
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
-
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 
@@ -21,6 +18,9 @@ export default {
   transform: {
     '.+\\.ts$': 'ts-jest',
   },
+
+  // Indicates whether the coverage information should be collected while executing the test
+  // collectCoverage: true,
 
   // All imported modules in your tests should be mocked automatically
   // automock: false,
