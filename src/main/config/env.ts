0ecf73d22fd5fe6ec965baa6daf3e89d84766b3c
Leoander Neves <leoander.neves@lab3dvlp.com>
2023-02-27 15:52:28 -0300

    feat: add ExpressRouteAdapter

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
   port: process.env.PORT || 5050,
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://admin:qzA0gTb2fGamjcjG@cluster0.zqdwuwl.mongodb.net/?retryWrites=true&w=majority'
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://mongodb_test:qzA0gTb2fGamjcjG@cluster0.wot4ahi.mongodb.net/?retryWrites=true&w=majority'
 }
