b5168f66d745caf7706f7286fed9281cb8550ba1
Leoander Neves <leoanderneves8@outlook.com>
2023-02-28 08:56:51 -0300

    test: ensure mongodb reconnect if client is down

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -12,7 +12,7 @@ describe('SignUp Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('account')
+    const accountCollection = await MongoHelper.getCollection('account')
     await accountCollection.deleteMany({})
   })
 
